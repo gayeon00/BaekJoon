3f718164e84a1f32bdcb84dea78c35f243ac75ec
gayeon00 <chlrkdus4819@gmail.com>
2023-03-16 23:12:56 +0900

    브론즈 문제 풀이

---
+++ Average4344/Average4344.cpp
@@ -0,0 +1,51 @@
+﻿// Average4344.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int C;
+vector<vector<int>> scores;
+
+void read_data() {
+	cin >> C;
+	for (int i = 0; i < C; i++) {
+		int N;
+		cin >> N;
+
+		int temp;
+		vector<int> tmpVector;
+		for (int j = 0; j < N; j++) {
+			cin >> temp;
+			tmpVector.push_back(temp);
+		}
+		scores.push_back(tmpVector);
+	}
+}
+
+void solve() {
+	for (auto scoreOfClass : scores) {
+
+		int avgScore, sumScores = 0;
+		for (auto eachScore : scoreOfClass) {
+			sumScores += eachScore;
+		}
+		avgScore = sumScores / scoreOfClass.size();
+
+		int countAboveAvg = 0;
+		for (auto eachScore : scoreOfClass) {
+			if (eachScore > avgScore) countAboveAvg++;
+		}
+
+		cout << fixed;
+		cout.precision(3);
+		cout << ((double)countAboveAvg/scoreOfClass.size())*100 << "%\n";
+	}
+}
+
+int main() {
+	read_data();
+	solve();
+}
---
+++ Word1157/Word1157.cpp
@@ -0,0 +1,44 @@
+﻿// Word1157.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <array>
+
+using namespace std;
+
+string word;
+char ans;
+
+void read_data() {
+	cin >> word;
+}
+
+
+void solve() {
+	array<int, 26> alphabet = { 0 };
+	for (char& i : word) { //복사하는 듯?
+		if (i >= 'a') alphabet[i - 'a']++;
+		else alphabet[i - 'A']++;
+	}
+
+	
+	int max = 0;
+	for (int i = 0; i < 26; i++) {
+		if (alphabet[i] == max) ans = '?';
+		else if (alphabet[i] > max) {
+			max = alphabet[i];
+			ans = 'A' + i;
+		}
+	}
+}
+
+void print_data() {
+	cout << ans;
+}
+
+int main() {
+	read_data();
+	solve();
+	print_data();
+}
