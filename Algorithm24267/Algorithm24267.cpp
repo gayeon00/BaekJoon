ffb79a29c5f637e31f10d70eb20e9510bc46af81
gayeon00 <chlrkdus4819@gmail.com>
2023-03-17 20:59:39 +0900

    알고리즘 문제1

---
+++ Algorithm24267/Algorithm24267.cpp
@@ -0,0 +1,13 @@
+﻿// Algorithm24267.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	long long n;
+	cin >> n;
+	cout << (n * (n - 1) * (n - 2) / 6) << endl;
+	cout << 3 << endl;
+}
