758c52971d6d104fc724087fdea3bfde3c6981c3
gayeon00 <chlrkdus4819@gmail.com>
2023-03-17 20:59:54 +0900

    알고리즘 문제2

---
+++ Algorithm24313/Algorithm24313.cpp
@@ -0,0 +1,25 @@
+﻿// Algorithm24313.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a0, a1;
+    int c;
+    int n0;
+
+    cin >> a0 >> a1;
+    cin >> c;
+    cin >> n0;
+
+    for (int i = n0; i < 10000; i++) {
+        if (a0 * i + a1 > c * i) {
+            cout << 0;
+            return 0;
+        }
+    }
+
+    cout << 1;
+    return 0;
+}
