d47b9b090c995c0f1d1a369e8b65328d565f8810
gayeon00 <chlrkdus4819@gmail.com>
2023-03-28 01:17:38 +0900

    버블 정렬

---
+++ SORT2750/SORT2750.cpp
@@ -0,0 +1,32 @@
+﻿// SORT2750.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A;
+
+    for (int i = 0; i < N; i++) {
+        int tmp;
+        cin >> tmp;
+        A.push_back(tmp);
+    }
+
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = 0; j < N - 1 - i; j++) {
+            if (A[j+1] < A[j]) {
+                int tmp = A[j + 1];
+                A[j + 1] = A[j];
+                A[j] = tmp;
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        cout << A[i] << '\n';
+    }
+}
