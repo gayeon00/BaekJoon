ee2f5b476cb77d0bac2e124469bc3809856bb82c
gayeon00 <chlrkdus4819@gmail.com>
2023-03-28 23:29:45 +0900

    1253 투포인터 복습

--- GOOD1253/GOOD1253.cpp
+++ GOOD1253/GOOD1253.cpp
@@ -1,60 +1,49 @@
-﻿// GOOD1253.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
-int N, cnt=0;
-vector<int> A;
-
-void read_data() {
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        int tmp;
-        cin >> tmp;
-        A.push_back(tmp);
-    }
-}
-
-void solve() {
-    sort(A.begin(), A.end());
-    
-    for (int k = 0; k < N; k++) {
-        int find = A[k];
-        int i = 0;
-        int j = N - 1;
-
-        while (i < j) {
-            if (A[i] + A[j] == find) {
-                if (i != k && j != k) {
-                    cnt++;
-                    break;
-                } else if (i == k) {
-                    i++;
-                } else if (j == k) {
-                    j--;
-                }
-            } else if (A[i] + A[j] > find) {
-                j--;
-            } else {
-                i++;
-            }
-        }
-    }
-}
-
-void print_data() {
-    cout << cnt;
-}
-
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    read_data();
-    solve();
-    print_data();
+	int N, count = 0;
+	vector<int> A;
+
+	cin >> N;
+	//일단 숫자 배열 입력 받기
+	for (int i = 0; i < N; i++) {
+		int tmp;
+		cin >> tmp;
+		A.push_back(tmp);
+	}
+
+	//숫자 배열 정렬하기
+	sort(A.begin(), A.end());
+
+	//k마다 좋은 수 찾기
+	for (int i = 0; i < N; i++) {
+		int start = 0; int end = N - 1;
+		while (start < end) {
+			if (A[start] + A[end] == A[i]) {
+				if (start != i && end != i) {
+					count++;
+					break;
+				} else if (start == i) {
+					start++;
+				} else if (end == i) {
+					end--;
+				}
+			} else if (A[start] + A[end] > A[i]) {
+				end--;
+			} else {
+				start++;
+			}
+		}
+	}
+
+	cout << count << '\n';
+	return 0;
 }
