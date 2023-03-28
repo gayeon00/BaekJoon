59d8525dc56e2cc553b13465e1a9b7c798ecc62d
gayeon00 <chlrkdus4819@gmail.com>
2023-03-28 22:59:06 +0900

    11659 구간합 복습

--- SUM11659/SUM11659.cpp
+++ SUM11659/SUM11659.cpp
@@ -1,28 +1,28 @@
-﻿// SUM11659.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
+﻿
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    int N, M;
-    cin >> N >> M;
-    int S[100001] = {};
+	int N, M;
+	cin >> N >> M;
+	int A[100001] = {};
 
-    for (int i = 1; i <= N; i++) {
-        int tmp;
-        cin >> tmp;
-        S[i] = S[i - 1] + tmp;
-    }
+	for (int i = 1; i <= N; i++) {
+		int tmp;
+		cin >> tmp;
+		A[i] = A[i - 1] + tmp;
+	}
 
-    for (int i = 0; i < M; i++) {
-        int start, end;
-        cin >> start >> end;
-        cout << S[end] - S[start - 1] << '\n';
-    }
+
+	for (int i = 0; i < M; i++) {
+		int u, v;
+		cin >> u >> v;
+		cout << A[v] - A[u - 1] << '\n';
+	}
 }
