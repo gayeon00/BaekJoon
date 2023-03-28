0f5cfc3f48bc56b4c6ec0d7f1b53bb8f202db941
gayeon00 <chlrkdus4819@gmail.com>
2023-03-29 03:16:15 +0900

    1541 stringstream getline 복습 - 주의: while (getline(ss, single, delimeter)) 일케 써야함

--- WHERE1541/WHERE1541.cpp
+++ WHERE1541/WHERE1541.cpp
@@ -1,55 +1,51 @@
-﻿// WHERE1541.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <string>
-#include <vector>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
-//모르겠는디?
-vector<string> split(string input, char delimeter) {
-    vector<string> result;
-    //string을 stringstream으로 바꿔주기
-    stringstream mystream(input);
-    string splitdata;
+vector<string> split(string& s, const char delimeter) {
+	stringstream ss(s);
+	string single;
+	vector<string> ps;
 
-    //mystream를 splitdata에 넣겠다. delimeter 기준으로 쪼갠것을 
-    while (getline(mystream, splitdata, delimeter)) {
-        result.push_back(splitdata);
-    }
-    return result;
+	while (getline(ss, single, delimeter)) {
+		ps.push_back(single);
+	}
+
+	return ps;
 }
 
-int mySum(string input) {
-    vector<string> result = split(input, '+');
-    int sum = 0;
-    for (auto i : result) {
-        sum += stoi(i);
-    }
-    return sum;
+int mySum(string ex) {
+	int sum = 0;
+	vector<string> vs = split(ex, '+');
+	for (string s : vs) {
+		sum += stoi(s);
+	}
+	return sum;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    string expression;
-    cin >> expression;
+	string ex;
+	cin >> ex;
 
-    vector<string> splitted = split(expression, '-');
-    int result = 0;
+	vector<string> splitted = split(ex, '-');
 
-    for (int i = 0; i < splitted.size(); i++) {
-        int sum = mySum(splitted[i]);
+	int result=0;
+	for (int i = 0; i < splitted.size(); i++) {
+		int sum = mySum(splitted[i]);
 
-        if (i == 0) {
-            result += sum;
-        } else {
-            result -= sum;
-        }
-    }
-    cout << result;
+		if (i == 0) {
+			result += sum;
+		} else {
+			result -= sum;
+		}
+	}
+
+	cout << result;
 }
