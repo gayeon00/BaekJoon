f7d8fb007e311668312fd92144c090cd59ccef8c
gayeon00 <chlrkdus4819@gmail.com>
2023-03-29 02:51:15 +0900

    11724 DFS 재귀 그래프 탐색 복습

--- LINKED11724/LINKED11724.cpp
+++ LINKED11724/LINKED11724.cpp
@@ -1,58 +1,46 @@
-﻿// LINKED11724.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
+
 using namespace std;
 
-int N, M;
-vector<vector<int>> A;
+vector < vector<int>> L;
 vector<bool> visited;
-int cnt = 0;
 
-void read_data() {
-    cin >> N >> M;
-    A.resize(N + 1);
-    visited = vector<bool>(N + 1, false);
-
-    for (int i = 0; i < M; i++) {
-        int u, v;
-        cin >> u >> v;
-        A[u].push_back(v);
-        A[v].push_back(u);
-    }
-}
-
-void DFS( int v){
-    if (visited[v]) return;
-
-    visited[v] = true;
-    for (int i : A[v]) {
-        if (visited[i] == false) {
-            DFS(i);
-        }
-    }
-}
-
-void solve() {
-    for (int i = 1; i <= N; i++) {
-        if (!visited[i]) { //방문 안했다면
-            cnt++;
-            DFS(i);
-        }
-    }
-}
-
-void print_data() {
-    cout << cnt;
+void DFS(int node) {
+	if (visited[node]) return;
+	visited[node] = true;
+	for (int i : L[node]) {
+		if (!visited[i]) DFS(i);
+	}
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    read_data();
-    solve();
-    print_data();
+	int N, M;
+	cin >> N >> M;
+
+	L.resize(N + 1);
+
+	//링크드 리스트로 그래프 입력받기
+	for (int i = 0; i < M; i++) {
+		int u, v;
+		cin >> u >> v;
+		L[u].push_back(v);
+		L[v].push_back(u);
+	}
+
+	int cnt = 0;
+
+	visited.resize(N + 1, false);
+	for (int i = 1; i <= N; i++) {
+		if (!visited[i]) {
+			DFS(i);
+			cnt++;
+		}
+	}
+
+	cout << cnt << '\n';
 }
