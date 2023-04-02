a581757ca9e67099bf8ca50747707d1efdcb3ee3
gayeon00 <chlrkdus4819@gmail.com>
2023-04-03 02:24:34 +0900

    1753 다익스트라 복습

--- SHORTEST1753/SHORTEST1753.cpp
+++ SHORTEST1753/SHORTEST1753.cpp
@@ -4,64 +4,59 @@
 
 using namespace std;
 
-int V, E, K;
-typedef pair<int, int> node;
-
-static vector<vector<node>> mlist;
-static vector<int> mdistance;
-static vector<bool> visited;
-static priority_queue<node, vector<node>, greater<node>> q;
+typedef pair<int, int> edge;
+static int V, E, K;
+static vector<vector<edge>> mlist; //인접리스트
+static vector<bool> visited;       //방문여부저장
+static vector<int> mdistance;      //노드까지 최단거리
+static priority_queue<edge, vector<edge>, greater<edge>> q;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-
-
 	cin >> V >> E >> K;
-
 	mlist.resize(V + 1);
-	mdistance.resize(V + 1);
-	fill(mdistance.begin(), mdistance.end(), 3000000);
 	visited.resize(V + 1);
 	fill(visited.begin(), visited.end(), false);
+	mdistance.resize(V + 1);
+	fill(mdistance.begin(), mdistance.end(), 3000000);
 
-
+	//인접리스트에 에지 정보 저장
 	for (int i = 0; i < E; i++) {
 		int u, v, w;
 		cin >> u >> v >> w;
-		mlist[u].push_back(make_pair(v, w));
+		mlist[u].push_back({ v,w });
 	}
 
-	q.push(make_pair(0, K));
+	//큐에 출발 노드 넣기{거리, 노드} -> 거리순으로 정렬돼야하니까
+	q.push({ 0,K });
 	mdistance[K] = 0;
 
 	while (!q.empty()) {
-		node now = q.top();
+		edge now = q.top();
 		q.pop();
-		int n_v = now.second;
-		int n_w = now.first;
-		if (visited[n_v]) continue;
-		visited[n_v] = true;
+		int now_n = now.second;
+		int now_v = now.first;
 
-		for (int i = 0; i < mlist[n_v].size(); i++) {
-			node tmp = mlist[n_v][i];
-			int next = tmp.first;
-			int value = tmp.second;
+		if (visited[now_n]) continue; //이미 방문한 노드는 스킵
+		visited[now_n] = true;
+		for (int i = 0; i < mlist[now_n].size(); i++) { //노드의 인접 노드들에 대해
+			edge next = mlist[now_n][i];
+			int node = next.first;
+			int value = next.second;
 
-			if (!visited[next] && (mdistance[n_v]+value < mdistance[next])) {
-				mdistance[next] = mdistance[n_v] + value;
-				q.push(make_pair(mdistance[next], next));
+			if (mdistance[now_n] + value < mdistance[node]) {
+				mdistance[node] = mdistance[now_n] + value;
+				q.push({ mdistance[node] , node });
 			}
 		}
 	}
 
 	for (int i = 1; i <= V; i++) {
-		if (visited[i]) {
-			cout << mdistance[i] << '\n';
-		} else {
-			cout << "INF" << '\n';
-		}
+		if (visited[i]) cout << mdistance[i] << '\n';
+		else			cout << "INF" << '\n';
 	}
 }
+
