ba32b539536e21fbbe1c36f481464902e48459c9
gayeon00 <chlrkdus4819@gmail.com>
2023-04-04 18:46:52 +0900

    구슬 빼기 복습

--- MARBLE13460/MARBLE13460.cpp
+++ MARBLE13460/MARBLE13460.cpp
@@ -1,27 +1,25 @@
-﻿// MARBLE13460.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <queue>
 
 using namespace std;
 
+static char Map[11][11];
+static bool visited[11][11][11][11];
+static int N, M;
+static int dx[] = { 1,-1,0,0 };
+static int dy[] = { 0,0,1,-1 };
+
 struct step {
-	int Rx;  int Ry;
-	int Bx; int By;
+	int rx, ry;
+	int bx, by;
 	int count;
 };
 
-int dx[] = { 1,-1,0,0 };
-int dy[] = { 0,0,1,-1 };
-bool visited[11][11][11][11];
-char Map[11][11];
-
-void move(int& rx, int& ry, int& distance, int& i) {
-	while (Map[rx + dx[i]][ry + dy[i]] != '#' && Map[rx][ry] != 'O') {  //현재 위치가 구멍이 되거나 다음위치가 벽이 될 때까지
+void move(int& rx, int& ry, int& rc, int& i) {
+	while (Map[rx+dx[i]][ry+dy[i]]!='#' && Map[rx][ry]!='O') {
 		rx += dx[i];
 		ry += dy[i];
-		distance++;
+		rc++;
 	}
 }
 
@@ -29,23 +27,24 @@ void BFS(int Rx, int Ry, int Bx, int By) {
 	queue<step> q;
 	q.push({ Rx, Ry, Bx, By, 0 });
 	visited[Rx][Ry][Bx][By] = true;
-
 	while (!q.empty()) {
-		int rx = q.front().Rx;
-		int ry = q.front().Ry;
-		int bx = q.front().Bx;
-		int by = q.front().By;
+		int rx = q.front().rx;
+		int ry = q.front().ry;
+		int bx = q.front().bx;
+		int by = q.front().by;
 		int count = q.front().count;
 		q.pop();
 
 		if (count >= 10) break;
+
 		for (int i = 0; i < 4; i++) {
 			int nrx = rx; int nry = ry;
 			int nbx = bx; int nby = by;
 			int rc = 0; int bc = 0;
-			int ncount = count + 1; //i방향으로 움직이니까
+			int ncount = count + 1;
 
-			move(nrx, nry, rc, i); //i방향으로 r움직이기
+
+			move(nrx, nry, rc, i);
 			move(nbx, nby, bc, i);
 
 			if (Map[nbx][nby] == 'O') continue;
@@ -53,7 +52,7 @@ void BFS(int Rx, int Ry, int Bx, int By) {
 				cout << ncount;
 				return;
 			}
-			if (nrx == nbx && nry == nby) {
+			if (nbx==nrx && nry==nby) {
 				if (rc > bc) {
 					nrx -= dx[i];
 					nry -= dy[i];
@@ -64,27 +63,26 @@ void BFS(int Rx, int Ry, int Bx, int By) {
 			}
 
 			if (visited[nrx][nry][nbx][nby]) continue;
+			q.push({ nrx, nry, nbx, nby, ncount });
 			visited[nrx][nry][nbx][nby] = true;
-			q.push({ nrx,nry,nbx,nby,ncount });
 		}
 	}
-	cout << -1 << '\n';
+
+	cout << -1;
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-
-	int N, M;
 	cin >> N >> M;
-	int Rx = 0; int Ry = 0; int Bx = 0; int By = 0;
+	int Rx=0, Ry=0, Bx=0, By=0;
 
+	//입력 받기
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
 			cin >> Map[i][j];
 			if (Map[i][j] == 'R') {
 				Rx = i; Ry = j;
-			} else if (Map[i][j] == 'B') {
+			}
+			if (Map[i][j] == 'B') {
 				Bx = i; By = j;
 			}
 		}
