d679bc2fd4a260bcfc1b10eff3ff96607e623187
gayeon00 <chlrkdus4819@gmail.com>
2023-04-13 22:57:57 +0900

    체스 복습

--- CHESS1018/CHESS1018.cpp
+++ CHESS1018/CHESS1018.cpp
@@ -1,75 +1,64 @@
-﻿// CHESS1018.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
-#include <vector>
-#include <array>
-#include <algorithm>
+﻿#include <iostream>
 #include <string>
-
 using namespace std;
-int M, N;
-//"WBWBWB""WBWBWB"이런 식
-string board[51];
 
-string w_board[8] = { "WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW" };
-string b_board[8] = { "BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB" };
+string wb = "WBWBWBWB";
+string bw = "BWBWBWBW";
 
-int w_first(int x, int y) {
-    int r = 0;
-    for (int i = 0; i < 8; i++)
-        for (int j = 0; j < 8; j++)
-            if (board[x + i][y + j] != w_board[i][j])
-                r++;
-
-    return r;
-}
-
-int b_first(int x, int y) {
-    int r = 0;
-    for (int i = 0; i < 8; i++)
-        for (int j = 0; j < 8; j++)
-            if (board[x + i][y + j] != b_board[i][j])
-                r++;
-
-
-    return r;
-}
+int N, M;
+string board[50];
 
 void read_data() {
-    cin >> N >> M;
-    for (int i = 0; i < N; i++) {
-        //Board[i]는 string
-        cin >> board[i];
-    }
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		cin >> board[i];
+	}
 }
 
+int countWhiteFirst(char tmp[][8]) {
+	int diff = 0;
+	string full[8] = { wb,bw,wb,bw,wb,bw,wb,bw };
 
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if (tmp[i][j] != full[i][j]) diff++;
+		}
+	}
+	return diff;
+}
+
+int countBlackFirst(char tmp[][8]) {
+	int diff = 0;
+	string full[8] = { bw,wb,bw,wb,bw,wb,bw,wb };
+
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if (tmp[i][j] != full[i][j]) diff++;
+		}
+	}
+	return diff;
+}
 
 void solve() {
-    int min = 10000;
-    int w = 0, b = 0;
-    for (int i = 0; i <= N - 8; i++) {
-        for (int j = 0; j <= M - 8; j++) {
+	int myMin = 64;
+	for (int i = 0; i <= N - 8; i++) {
+		for (int j = 0; j <= M - 8; j++) {
+			char tmp[8][8];
 
-            w = w_first(i, j);
-            b = b_first(i, j);
+			for (int k = 0; k < 8; k++) {
+				for (int t = 0; t < 8; t++) {
+					tmp[k][t] = board[i + k][j + t];
+				}
+			}
+			myMin = min(myMin, min(countWhiteFirst(tmp), countBlackFirst(tmp)));
+		}
+	}
 
-            if (w < b)
-                min = (w < min) ? w : min;
-            else
-                min = (b < min) ? b : min;
-        }
-    }
-
-    cout << min << "\n";
+	cout << myMin;
 }
 
-
-
 int main() {
-    read_data();
-    solve();
-    //print_data();
-    
+	read_data();
+	solve();
 }
