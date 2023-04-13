efae04dcdbfda5281e04e9408365cd667306ce6e
gayeon00 <chlrkdus4819@gmail.com>
2023-04-14 00:11:46 +0900

    재귀로 순열 구하기 DFS

---
+++ enc_temp_folder/f25298ee4653e9f127256df91a1e2af9/NM15649.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int N, M;
+bool visited[9] = { false };
+void DFS(int i, int count) {
+	visited[i] = true;
+	
+	if (count == M) {
+		cout << '\n';
+		return;
+	}
+
+	for (int j = i + 1; j <= N; j++) {
+		if (visited[j]) continue;
+
+		cout << i << " ";
+		DFS(j, count + 1);
+	}
+
+	visited[i] = false;
+}
+
+int main() {
+	cin >> N >> M;
+
+	for (int i = 1; i <= N; i++) {
+		DFS(i, 1);
+	}
+}
