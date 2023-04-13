699712cca62da30fa5e963505e61f8a5bd890d5b
gayeon00 <chlrkdus4819@gmail.com>
2023-04-13 12:30:03 +0900

    복습

--- Goldbach9020/Goldbach9020.cpp
+++ Goldbach9020/Goldbach9020.cpp
@@ -1,59 +1,50 @@
-﻿// Goldbach9020.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
+#include <memory.h>
+
 using namespace std;
-typedef pair<int, int> pii;
-const int N = 10000;
 
-int T;
-vector<int> targetNumbers;
-vector<bool> is_prime(N + 1, true);
+vector<int> primes(int n) {
+	bool is_prime[10001] = { true };
+	memset(is_prime, true, 10001);
+	is_prime[0] = false, is_prime[1] = false;
 
-void read_data() {
-    cin >> T;
-    int tmp;
-    for (int i = 0; i < T; i++) {
-        cin >> tmp;
-        targetNumbers.push_back(tmp);
-    }
+	for (int i = 2; i * i <= n; i++) {
+		if (!is_prime[i]) continue;
+		
+		for (int j = i * i; j <= n; j+=i) {
+			is_prime[j] = false;
+		}
+	}
+
+	vector<int> primes_list;
+	for (int i = 0; i <= n; i++) {
+		if (is_prime[i]) primes_list.push_back(i);
+	}
+	return primes_list;
 }
 
-void primes(int n) {
-    
-    is_prime[0] = false, is_prime[1] = false;
-
-    for (int i = 2; i * i <= n; i++) {
-        if (is_prime[i]) {
-            for (int j = i * i; j <= n; j += i) {
-                is_prime[j] = false;
-            }
-        }
-    }
-}
-
-void isGoldBach(int n, vector< bool>& primes_list) {
-    for (int i = 0; i < n / 2; i++) {
-        if (primes_list[n / 2 - i] && primes_list[n / 2 + i]) {
-            cout << (n / 2 - i) << " " << (n / 2 + i) << '\n';
-            break;
-        }
-    }
-}
-
-
-void solve() {
-    primes(N);
-    for (auto i : targetNumbers) {
-        isGoldBach(i, is_prime);
-    }
-
+void getTwoNumbers(int n, vector<int>& prime_list) {
+	int min = 10001;
+	int minI = 0, minJ = 0;
+	for (auto i : prime_list) {
+		for (auto j : prime_list) {
+			if (i + j == n && abs(i - j) < min) {
+				min = abs(i - j);
+				minI = i, minJ = j;
+			}
+		}
+	}
+	cout << minI << " " << minJ << '\n';
 }
 
 int main() {
-    read_data();
-    solve();
-    
-    
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		int n;
+		cin >> n;
+		vector<int> prime_list = primes(n);
+		getTwoNumbers(n, prime_list);
+	}
 }
--- Statistics2108/Statistics2108.cpp
+++ Statistics2108/Statistics2108.cpp
@@ -1,60 +1,62 @@
-﻿// Statistics2108.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <queue>
 #include <vector>
-#include <map>
 #include <array>
 #include <algorithm>
 #include <cmath>
+
 using namespace std;
 
-double arithmeticMean(vector<int>& numbers) {
-	int sum = 0;
-	for (auto i : numbers) {
-		sum += i;
+int sansul(vector<int>& numbers, int N) {
+	double result = 0;
+	for (auto i = 0; i <N ; i++) {
+		result += numbers[i];
 	}
-	int roundNum = round((double)sum / numbers.size());
-	return (roundNum==0?0:roundNum);
+	result = round(result / N);
+	return result == 0 ? 0 : result;
 }
 
-int centerNumber(vector<int>& numbers) {
-	sort(numbers.begin(), numbers.end());
-	return numbers[numbers.size() / 2];
+int joongang(vector<int>& numbers, int N) {
+	return numbers[N / 2];
 }
 
-int mostFrequentNumber(array<int,8001>& countNumbers) {
-	auto maxIndex = max_element(countNumbers.begin(), countNumbers.end());
-	vector<int> mostFrequentNumbers;
-	for (int i = 0; i < countNumbers.size(); i++) {
-		if (countNumbers[i] == *maxIndex) {
-			mostFrequentNumbers.push_back(i - 4000);
+int choibin(array<int, 8001>& countNumbers, int N) {
+	auto maxIter = max_element(countNumbers.begin(), countNumbers.end());
+	vector<int> choibinNumbers;
+	for (int i = 0; i < 8001; i++) {
+		if (countNumbers[i] == *maxIter) {
+			choibinNumbers.push_back(i - 4000);
 		}
 	}
-	return mostFrequentNumbers.size()==1?mostFrequentNumbers.front():*(mostFrequentNumbers.begin()+1);
-	
+	return choibinNumbers.size() == 1 ? choibinNumbers[0] : choibinNumbers[1];
 }
 
-int rangeBetMaxMin(vector<int>& numbers) {
-	return numbers.back() - numbers.front();
+int range(vector<int>& numbers, int N) {
+	return numbers[N - 1] - numbers[0];
 }
+
 
 int main() {
 	int N;
 	cin >> N;
-
-	vector<int> numbers;
-	int tmp;
+	priority_queue<int, vector<int>, greater<int>> q;
 	array<int, 8001> countNumbers = { 0 };
-	while (N--) {
+	for (int i = 0; i < N; i++) {
+		int tmp;
 		cin >> tmp;
-		numbers.push_back(tmp);
+		q.push(tmp);
 		countNumbers[tmp + 4000]++;
 	}
 
-	cout << arithmeticMean(numbers) << '\n';
-	cout << centerNumber(numbers) << '\n';
-	cout << mostFrequentNumber(countNumbers) << '\n';
-	cout << rangeBetMaxMin(numbers) << '\n';
+	vector<int> numbers;
+	while (!q.empty()) {
+		numbers.push_back(q.top());
+		q.pop();
+	}
+
+	cout << sansul(numbers, N) << '\n';
+	cout << joongang(numbers, N) << '\n';
+	cout << choibin(countNumbers, N) << '\n';
+	cout << range(numbers, N) << '\n';
 
 }
