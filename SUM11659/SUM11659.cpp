e7032ed0ee46934e8be49f41a92098ebfea99da2
gayeon00 <chlrkdus4819@gmail.com>
2023-04-14 20:12:51 +0900

    구간합 복습

--- SUM11659/SUM11659.cpp
+++ SUM11659/SUM11659.cpp
@@ -1,28 +1,28 @@
-﻿
-#include <iostream>
-#include <vector>
-
+﻿#include <iostream>
 using namespace std;
 
 int main() {
+	/*
+	i번째 부터 j번째 수까지 합 구하기
+	gugan(i,j) = until(j)-sum(i-1)
+	*/
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
 	int N, M;
 	cin >> N >> M;
-	int A[100001] = {};
 
+	int until[100001] = {};
 	for (int i = 1; i <= N; i++) {
 		int tmp;
 		cin >> tmp;
-		A[i] = A[i - 1] + tmp;
+		until[i] = until[i - 1] + tmp;
 	}
 
-
 	for (int i = 0; i < M; i++) {
-		int u, v;
-		cin >> u >> v;
-		cout << A[v] - A[u - 1] << '\n';
+		int x, y;
+		cin >> x >> y;
+		cout << until[y] - until[x-1] << '\n';
 	}
 }
