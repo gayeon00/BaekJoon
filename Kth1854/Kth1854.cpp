3317ed639ca403c1963d07ee9c14897cbcf7fc10
gayeon00 <chlrkdus4819@gmail.com>
2023-04-18 18:23:22 +0900

    K번째 최단거리 뿌심;;;

--- Kth1854/Kth1854.cpp
+++ Kth1854/Kth1854.cpp
@@ -1,52 +1,63 @@
 ﻿#include <iostream>
 #include <queue>
+#include <vector>
 
 using namespace std;
-typedef pair<int, int> node;
 
-static int N, M, K;
-static int W[1001][1001];
-static priority_queue<int> distQueue[1001];
+typedef pair<int, int> edge;
+int n, m, k;
+//top(젤 큰놈)이랑 들어오는 얘랑 비교해서 들어오는 얘가 더 작으면 top 꺼내버리고 들어오는 얘 넣기
+vector<vector<edge>> mlist;
+vector<priority_queue<int>> mdistance;
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+void read_data() {
+	cin >> n >> m >> k;
+	
+	mlist.resize(n + 1);
+	mdistance.resize(n + 1);
 
-	cin >> N >> M >> K;
-	for (int i = 0; i < M; i++) {
+	for (int i = 0; i < m; i++) {
 		int a, b, c;
 		cin >> a >> b >> c;
-		W[a][b] = c;
+		mlist[a].push_back({ b,c });
 	}
+}
 
-	priority_queue < node, vector<node>, greater<node>> pq;
-	pq.push({ 0,1 });
-	distQueue[1].push(0);
+void solve(int start) {
+	priority_queue<edge, vector<edge>, greater<edge>> q;
 
-	while (!pq.empty()) {
-		node u = pq.top();
-		pq.pop();
+	q.push({ 0,1 });
+	mdistance[1].push(0);
 
-		for (int adjNode = 1; adjNode <= N; adjNode++) {
-			if (W[u.second][adjNode]!=0) { //현재 노드와 타켓 노드사이 거리
-				if (distQueue[adjNode].size() < K) {
-					pq.push({ u.first + W[u.second][adjNode] , adjNode });
-					distQueue[adjNode].push(u.first + W[u.second][adjNode]);
-				} else if(distQueue[adjNode].top() > u.first + W[u.second][adjNode]) {
-					distQueue[adjNode].pop();
-					pq.push({ u.first + W[u.second][adjNode] , adjNode });
-					distQueue[adjNode].push(u.first + W[u.second][adjNode]);
-				}
+	while (!q.empty()) {
+		edge curr = q.top();
+		q.pop();
+
+		for (auto i : mlist[curr.second]) {
+			int next = i.first;
+			int value = i.second;
+
+			//우선순위 큐 배열이 다 채워지지 않았을 경우
+			if (mdistance[next].size() < k) {
+				mdistance[next].push({ curr.first + value });
+				q.push({ curr.first + value, next });
+			}
+			//우선순위 큐 배열이 k개로 다 채워졌을 경우
+			else if (mdistance[next].top() > curr.first + value) {
+				mdistance[next].pop();
+				mdistance[next].push({ curr.first + value });
+				q.push({ curr.first + value, next });
 			}
 		}
 	}
 
-	for (int i = 1; i <= N; i++) {
-		if (distQueue[i].size() == K) {
-			cout << distQueue[i].top() << '\n';
-		} else {
-			cout << -1 << '\n';
-		}
+	for (int i = 1; i <= n; i++) {
+		if (mdistance[i].size() == k) cout << mdistance[i].top() << '\n';
+		else cout << -1 << '\n';
 	}
+}
+
+int main() {
+	read_data();
+	solve(1);
 }
