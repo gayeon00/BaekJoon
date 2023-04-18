4aae1f66c4dd1648a1c9ba16e951e23281b5ceb0
gayeon00 <chlrkdus4819@gmail.com>
2023-04-18 17:30:45 +0900

    다익스트라 복습...다시....

--- SHORTEST1753/SHORTEST1753.cpp
+++ SHORTEST1753/SHORTEST1753.cpp
@@ -1,62 +1,56 @@
 ﻿#include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
+#include <limits.h>
 
 using namespace std;
 
 typedef pair<int, int> edge;
-static int V, E, K;
-static vector<vector<edge>> mlist; //인접리스트
-static vector<bool> visited;       //방문여부저장
-static vector<int> mdistance;      //노드까지 최단거리
-static priority_queue<edge, vector<edge>, greater<edge>> q;
+int V, E, K;
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+vector<vector<edge>> mlist;
+vector<int> mdistance;
 
+void read_data() {
 	cin >> V >> E >> K;
-	mlist.resize(V + 1);
-	visited.resize(V + 1);
-	fill(visited.begin(), visited.end(), false);
-	mdistance.resize(V + 1);
-	fill(mdistance.begin(), mdistance.end(), 3000000);
 
-	//인접리스트에 에지 정보 저장
+	mlist.resize(V + 1);
+	mdistance.resize(V + 1, INT_MAX);
+
 	for (int i = 0; i < E; i++) {
 		int u, v, w;
 		cin >> u >> v >> w;
 		mlist[u].push_back({ v,w });
 	}
+}
 
-	//큐에 출발 노드 넣기{거리, 노드} -> 거리순으로 정렬돼야하니까
-	q.push({ 0,K });
+void solve(int K) {
+	priority_queue<edge, vector<edge>, greater<edge>> q;
 	mdistance[K] = 0;
+	q.push({ 0,K });
 
 	while (!q.empty()) {
-		edge now = q.top();
+		edge curr = q.top();
 		q.pop();
-		int now_n = now.second;
-		int now_v = now.first;
 
-		if (visited[now_n]) continue; //이미 방문한 노드는 스킵
-		visited[now_n] = true;
-		for (int i = 0; i < mlist[now_n].size(); i++) { //노드의 인접 노드들에 대해
-			edge next = mlist[now_n][i];
-			int node = next.first;
-			int value = next.second;
+		for (auto i : mlist[curr.second]) {
+			int next = i.first;
+			int value = i.second;
 
-			if (mdistance[now_n] + value < mdistance[node]) {
-				mdistance[node] = mdistance[now_n] + value;
-				q.push({ mdistance[node] , node });
+			if (mdistance[next] > mdistance[curr.second] + value) {
+				mdistance[next] = mdistance[curr.second] + value;
+				q.push({ mdistance[next],next });
 			}
 		}
 	}
 
 	for (int i = 1; i <= V; i++) {
-		if (visited[i]) cout << mdistance[i] << '\n';
-		else			cout << "INF" << '\n';
+		if (mdistance[i] != INT_MAX) cout << mdistance[i] << '\n';
+		else cout << "INF" << '\n';
 	}
-}
 
+}
+int main() {
+	read_data();
+	solve(K);
+}
