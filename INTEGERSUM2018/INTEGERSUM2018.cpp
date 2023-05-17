08359a150c928b8e295aba3cbb8523e1aad79734
gayeon00 <chlrkdus4819@gmail.com>
2023-05-18 02:12:35 +0900

    투포인터, 연속된 숫자의 합이 N인 구간 갯수 구하기

---
+++ INTEGERSUM2018/INTEGERSUM2018.cpp
@@ -0,0 +1,32 @@
+﻿// INTEGERSUM2018.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int count = 1;
+    int start_index = 1;
+    int end_index = 1;
+    int sum = 1;
+
+    while (end_index != N) {
+        if (sum == N) {
+            end_index++;
+            sum += end_index;
+            count++;
+        } else if (sum < N) {
+            end_index++;
+            sum += end_index;
+        } else {
+            sum -= start_index;
+            start_index++;
+        }
+    }
+
+    cout << count;
+
+}
