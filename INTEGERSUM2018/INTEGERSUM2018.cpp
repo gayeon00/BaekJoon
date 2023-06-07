c31735468f92b4187a3d4f2a08a275136a8ae0c8
gayeon00 <chlrkdus4819@gmail.com>
2023-06-07 23:06:33 +0900

    투포인터 -> 인덱스 움직일 때마다 sum도 업데이트해줘야함!

--- INTEGERSUM2018/INTEGERSUM2018.cpp
+++ INTEGERSUM2018/INTEGERSUM2018.cpp
@@ -1,32 +1,26 @@
-﻿// INTEGERSUM2018.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
-#include <vector>
+﻿#include <iostream>
 using namespace std;
 
 int main() {
-    int N;
-    cin >> N;
-    int count = 1;
-    int start_index = 1;
-    int end_index = 1;
-    int sum = 1;
+	int start_index = 1, end_index = 1;
+	int sum = 1, count = 1;
+	int N;
 
-    while (end_index != N) {
-        if (sum == N) {
-            end_index++;
-            sum += end_index;
-            count++;
-        } else if (sum < N) {
-            end_index++;
-            sum += end_index;
-        } else {
-            sum -= start_index;
-            start_index++;
-        }
-    }
+	cin >> N;
 
-    cout << count;
+	while (end_index != N) {
+		if (sum == N) {
+			count++;
+			end_index++;
+			sum += end_index;
+		} else if (sum < N) {
+			end_index++;
+			sum += end_index;
+		} else {
+			sum -= start_index;
+			start_index++;
+		}
+	}
 
+	cout << count;
 }
