6987f7ce556df1bd47ba32ec8676ec1f64121d07
gayeon00 <chlrkdus4819@gmail.com>
2023-06-08 22:09:19 +0900

    구간합 5 복습

--- GUGAN11660/GUGAN11660.cpp
+++ GUGAN11660/GUGAN11660.cpp
@@ -1,30 +1,29 @@
-﻿// GUGAN11660.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
-
+﻿#include <iostream>
 using namespace std;
 
-int S[1026][1026] = { 0 };
 
-int main(){
+int S[1025][1025] = { 0 };
+
+int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
 	int N, M;
 	cin >> N >> M;
+
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
 			int tmp;
 			cin >> tmp;
+
 			S[i][j] = S[i - 1][j] + S[i][j - 1] - S[i - 1][j - 1] + tmp;
 		}
 	}
 
-	for (int i = 0; i < M; i++) {
-		int a, b, c, d;
-		cin >> a >> b >> c >> d;
-		cout << S[c][d] - S[a - 1][d] - S[c][b - 1] + S[a - 1][b - 1]<<'\n';
+	for (int j = 0; j < M; j++) {
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		cout << S[x2][y2] - S[x2][y1 - 1] - S[x1 - 1][y2] + S[x1 - 1][y1 - 1] << '\n';
 	}
 }
