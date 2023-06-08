ed0c501885b3e8c0b8e03d6bc924da8a0642a7a7
gayeon00 <chlrkdus4819@gmail.com>
2023-06-08 23:42:49 +0900

    좋은 수 복습

--- GOOD1253/GOOD1253.cpp
+++ GOOD1253/GOOD1253.cpp
@@ -1,51 +1,42 @@
 ﻿#include <iostream>
+#include <vector>
 #include <algorithm>
-
 using namespace std;
 
 int main() {
-	/*
-	수를 배열로 나타내고,
-	맨 앞을 가리키는 포인터1, 타겟 숫자를 가리키는 포인터2
-	while(포인터1이 포인터2보다 앞에 있을 동안){
-		if(포인터1+포인터2가 타겟이면) {
-			count++;
-			while문 나가기!
-		}
-	}
-	*/
 	int N;
 	cin >> N;
 
-	int numbers[2000];
+	vector<int> A(N, 0);
 	for (int i = 0; i < N; i++) {
-		cin >> numbers[i];
+		cin >> A[i];
 	}
-	
-	sort(numbers, numbers+N);
+
+	sort(A.begin(), A.end());
 
 	int cnt = 0;
-	for (int i = 0; i < N; i++) {
-		int a = 0, b = N - 1;
-		while (a < b) {
-			if (numbers[a] + numbers[b] == numbers[i]) {
-				if (a != i && b != i) {
+	for (int k = 0; k < N; k++) {
+		int target = A[k];
+		int i = 0;
+		int j = N - 1;
+
+		while (i < j) {
+			if (A[i] + A[j] == target) {
+				if (i != k && j != k) {
 					cnt++;
 					break;
-				} else if (a == i) {
-					a++;
-				} else if (b == i) {
-					b--;
+				} else if (i == k) {
+					i++;
+				} else if (j == k) {
+					j--;
 				}
-			} else if (numbers[a] + numbers[b] > numbers[i]) {
-				b--;
+			} else if (A[i] + A[j] < target) {
+				i++;
 			} else {
-				a++;
+				j--;
 			}
 		}
 	}
 
 	cout << cnt;
-
-
 }
