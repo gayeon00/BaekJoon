754ded94c1c387f915a9cf77dbecd111a9fafe4f
gayeon00 <chlrkdus4819@gmail.com>
2023-06-12 22:29:11 +0900

    줄끝 정규화?

--- DNA12891/DNA12891.cpp
+++ DNA12891/DNA12891.cpp
@@ -23,20 +23,28 @@ int main() {
 		}
 	}
 
+	//초기상태. 문자열 0번째부터 P번째까지 알파벳 카운트를 더해줌
 	for (int i = 0; i < P; i++) {
 		Add(A[i]);
 	}
 
+	//초기상태가 조건을 만족하면 result++
 	if (checkSecret == 4) {
 		result++;
 	}
 
 	//슬라이딩 윈도우 처리
+	//초기상태가 P-1번째까지 처리돼있으니까 P번째로 늘어나기
 	for (int i = P; i < S; i++) {
+		//앞 인덱스는 {마지막인덱스 - 길이} 해줌됨
 		int j = i - P;
+		//뒤 인덱스는 알파벳 카운트 더해주고
 		Add(A[i]);
+		//앞 인덱스는 알파벳 카운트 빼줌
 		Remove(A[j]);
 
+
+		//한번 하고 조건 만족하면 result++;
 		if (checkSecret == 4) {
 			result++;
 		}
@@ -79,9 +87,11 @@ void Add(char c) {
 void Remove(char c) {
 	switch (c) {
 	case 'A':
+		//빠질 놈이 조건을 만족하고 있었다면 갯수--
 		if (myArr[0] == checkArr[0]) {
 			checkSecret--;
 		}
+		//빼주기
 		myArr[0]--;
 		break;
 	case 'C':
