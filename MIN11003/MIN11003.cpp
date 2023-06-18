483500e76cade2d0305626ef7cd1f612a0b55dcd
gayeon00 <chlrkdus4819@gmail.com>
2023-06-19 00:46:48 +0900

    윈도우 슬라이딩: 몇번째 숫자인지, 그 숫자의 인덱스를 적극 활용한다!!

--- MIN11003/MIN11003.cpp
+++ MIN11003/MIN11003.cpp
@@ -1,35 +1,33 @@
 ﻿#include <iostream>
-#include <deque>
-#include <algorithm>
 #include <vector>
-
+#include <deque>
 using namespace std;
 
+int N, L;
+deque<pair<int, int>> q;
 int main() {
-	/*
-	슬라이딩 윈도우!
-	덱에 {인덱스, 숫자}로 집어 넣기
-	덱에 넣을 때, 들어가는 숫자랑 맨 뒤에 있는 숫자 비교해서
-	맨 뒤에 있는 숫자가 더크다면 빼주기
-	=> 오름차순 정렬가능! => 최솟값 구하는 거니까 나중에 그냥 맨 앞값 가져오면 됨!
-	그리고 맨 뒤에 인덱스 - 맨 앞 인덱스 < L이어야함! (윈도우 크기 때문)
-	*/
-	int N, L;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	cin >> N >> L;
 
-	deque<pair<int,int>> dq;
+	int tmp;
 	for (int i = 1; i <= N; i++) {
-		int tmp;
 		cin >> tmp;
-		while (dq.size() && dq.back().second > tmp) {
-			dq.pop_back();
-		}
-		dq.push_back({ i,tmp });
-		if (dq.back().first - dq.front().first >= L) {
-			dq.pop_front();
+
+		if (q.size() == 0) {
+			q.push_back({i,tmp});
+		} else {
+			if (i - q.front().first >= L) {
+				q.pop_front();
+			}
+			while (q.size() != 0 && q.back().second > tmp) {
+				q.pop_back();
+			}
+			q.push_back({ i,tmp });
 		}
 
-		cout << dq.front().second << " ";
+		cout << q.front().second << " ";
 	}
-	
 }
