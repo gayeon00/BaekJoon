8781481119c6f0c8604a3dc410d4c6c1ae7d5578
gayeon00 <chlrkdus4819@gmail.com>
2023-06-19 01:29:14 +0900

    스택 주석 달기

--- STACK1874/STACK1874.cpp
+++ STACK1874/STACK1874.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+//꺼내야될 숫자 tmp가 stack의 top보다 크거나 같은 수여야 stack의 성질을 만족함
 int main()
 {
     int N;
@@ -11,28 +12,33 @@ int main()
 
     vector<int> v;
     vector<char> result;
+    //1부터 N까지의 수열 숫자
     int j = 1;
     for (int i = 0; i < N; i++) {
         int tmp;
         cin >> tmp;
 
+        //stack이 비었으면 수열 숫자 넣어주기
         if (v.size() == 0) {
             v.push_back(j++);
             result.push_back('+');
         }
-        if (v.back() > tmp) {
 
-        }
+        //"stack의 top"이 "꺼내야될 숫자"보다 크거나 같을 때까지 수열 숫자 넣기
         while (v.back() < tmp) {
             v.push_back(j++);
             result.push_back('+');
         }
+
+        //"stack의 top"이 "꺼내야될 숫자"보다 크거나 같아지면
+        //같아야만 pop할 수 있음
         if (v.back() == tmp) {
             v.pop_back();
             result.push_back('-');
+            //더 크다면 만들 수 없는 수임!
         } else {
             cout << "NO";
-            return 0;
+            return;
         }
     }
 
