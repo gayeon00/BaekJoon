4d8802a4ef8236bd66f0241e2d37b49bd7bf68ce
gayeon00 <chlrkdus4819@gmail.com>
2023-06-20 20:50:15 +0900

    priorityqueue compare struct

---
+++ ABSHEAP11286/ABSHEAP11286.cpp
@@ -0,0 +1,40 @@
+﻿// ABSHEAP11286.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+struct compare {
+    bool operator()(int& a, int& b) {
+        if (abs(a) == abs(b)) {
+            return a > b;
+        }
+        return abs(a) > abs(b);
+    }
+};
+
+int main() {
+    int N;
+    cin >> N;
+
+    priority_queue<int, vector<int>, compare> pq;
+
+    for (int i = 0; i < N; i++) {
+        int tmp;
+        cin >> tmp;
+
+        if (tmp == 0) {
+            if (pq.empty()) {
+                cout << 0 << '\n';
+            } else {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        } else {
+            //queue에 tmp넣기
+            pq.push(tmp);
+        }
+    }
+}
