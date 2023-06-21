f7d39a37d5a8bf1bdb8fd4351e89806e93572b7e
gayeon00 <chlrkdus4819@gmail.com>
2023-06-22 02:07:20 +0900

    버블정렬

--- SORT2750/SORT2750.cpp
+++ SORT2750/SORT2750.cpp
@@ -2,31 +2,61 @@
 //
 
 #include <iostream>
-#include <vector>
 using namespace std;
 
+int A[1001];
 int main() {
-    int N;
-    cin >> N;
-    vector<int> A;
+	int N;
+	cin >> N;
 
-    for (int i = 0; i < N; i++) {
-        int tmp;
-        cin >> tmp;
-        A.push_back(tmp);
-    }
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
+	}
 
-    for (int i = 0; i < N - 1; i++) {
-        for (int j = 0; j < N - 1 - i; j++) {
-            if (A[j+1] < A[j]) {
-                int tmp = A[j + 1];
-                A[j + 1] = A[j];
-                A[j] = tmp;
-            }
-        }
-    }
+	for (int i = 0; i < N - 1; i++) {
+		for (int j = 0; j < N - 1 - i; j++) {
+			if (A[j] > A[j + 1]) {
+				int tmp = A[j];
+				A[j] = A[j + 1];
+				A[j + 1] = tmp;
+			}
+		}
+	}
 
-    for (int i = 0; i < N; i++) {
-        cout << A[i] << '\n';
-    }
+	for (int i = 0; i < N; i++) {
+		cout << A[i] << '\n';
+	}
 }
+
+
+
+
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main() {
+//    int n;
+//    cin >> n;
+//    vector<int> a;
+//
+//    for (int i = 0; i < n; i++) {
+//        int tmp;
+//        cin >> tmp;
+//        a.push_back(tmp);
+//    }
+//
+//    for (int i = 0; i < n - 1; i++) {
+//        for (int j = 0; j < n - 1 - i; j++) {
+//            if (a[j+1] < a[j]) {
+//                int tmp = a[j + 1];
+//                a[j + 1] = a[j];
+//                a[j] = tmp;
+//            }
+//        }
+//    }
+//
+//    for (int i = 0; i < n; i++) {
+//        cout << a[i] << '\n';
+//    }
+//}
