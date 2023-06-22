8e1086f7a0d0dbda183a08362f7526a46f633e81
gayeon00 <chlrkdus4819@gmail.com>
2023-06-23 01:17:42 +0900

    버블 정렬 몇회차 만에 정렬이 되는지? == 내부 for문이 몇번이나 도는지? == 정렬 전 순열과 정렬 후 순열에 대해 왼쪽으로 옮겨간 횟수의 최댓값

---
+++ BUBBLE1377/BUBBLE1377.cpp
@@ -0,0 +1,50 @@
+﻿// BUBBLE1377.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    vector<pair<int, int>> A(N);
+
+    for (int i = 0; i < N; i++) {
+        //숫자
+        cin >> A[i].first;
+        //숫자의 인덱스
+        A[i].second = i;
+    }
+
+    sort(A.begin(), A.end());
+    int Max = 0;
+
+    for (int i = 0; i < N; i++) {
+        //A[i].second: 정렬 전 인덱스
+        //i: 정렬 후 인덱스
+        if (Max < A[i].second - i) {
+            Max = A[i].second - i;
+        }
+    }
+
+    cout << Max + 1;
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
