018c8459359bbc0f9fd50034d47637efa952a719
gayeon00 <chlrkdus4819@gmail.com>
2023-07-04 21:02:24 +0900

    기수정렬

---
+++ RADIX10989/RADIX10989.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	int A[10001] = { 0 };
+	int tmp;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> tmp;
+
+		A[tmp]++;
+	}
+
+	for (int i = 0; i <= 10000; i++) {
+		if (A[i] != 0) {
+			for (int j = 0; j < A[i]; j++) {
+				cout << i << '\n';
+			}
+		}
+	}
+}
