09f0b0d95610be9def74e79d8a9e0a17b928790d
gayeon00 <chlrkdus4819@gmail.com>
2023-07-18 01:38:16 +0900

    DFS, BFS

--- DFSBFS1260/DFSBFS1260.cpp
+++ DFSBFS1260/DFSBFS1260.cpp
@@ -1,69 +1,69 @@
 ﻿#include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
-
+#include <queue>
 using namespace std;
 
-vector< vector<int>> L;
+int N, M, V;
+vector<vector<int>> A;
 vector<bool> visited;
+queue<int> q;
 
+void DFS(int v, vector<bool>& visited) {
+	visited[v] = true;
+	cout << v << " ";
 
-void DFS(int node) {
-	cout << node << ' ';
-	visited[node] = true;
-
-	for (int i : L[node]) {
-		if (!visited[i]) {
-			DFS(i);
+	for (auto next : A[v]) {
+		if (!visited[next]) {
+			DFS(next, visited);
 		}
 	}
+
+	return;
 }
 
-void BFS(int node) {
-	queue<int> myQueue;
-	myQueue.push(node);
-	visited[node] = true;
+void BFS(int v, vector<bool>& visited) {
+	q.push(v);
 
-	while (!myQueue.empty()) {
-		int now_node = myQueue.front();
-		myQueue.pop();
-		cout << now_node << " ";
-		for (int i : L[now_node]) {
-			if (!visited[i]) {
-				myQueue.push(i);
-				visited[i] = true;
+	while (!q.empty()) {
+		int now = q.front();
+
+		if (!visited[now]) {
+			visited[now] = true;
+			cout << now << " ";
+			for (auto next : A[now]) {
+				if (!visited[next]) {
+					q.push(next);
+				}
 			}
 		}
+		q.pop();
 	}
+
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	int N, M, V;
 	cin >> N >> M >> V;
 
-	L.resize(N + 1);
-	//인접 리스트 만들기
+	A.resize(N + 1);
+	visited.resize(N + 1);
+
 	for (int i = 0; i < M; i++) {
 		int u, v;
 		cin >> u >> v;
-		L[u].push_back(v);
-		L[v].push_back(u);
+
+		A[u].push_back(v);
+		A[v].push_back(u);
 	}
 
-	//정렬
+	// 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문하기 위해 정렬
 	for (int i = 1; i <= N; i++) {
-		sort(L[i].begin(), L[i].end());
+		sort(A[i].begin(), A[i].end());
 	}
 
-	visited = vector<bool>(N + 1, false);
-	DFS(V);
+	DFS(V, visited);
 	cout << '\n';
-	fill(visited.begin(), visited.end(), false);
-	BFS(V);
-
+	visited.clear();
+	visited.resize(N + 1, false);
+	BFS(V, visited);
 }
