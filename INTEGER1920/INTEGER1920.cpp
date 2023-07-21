e55dee66d2926d6f6fb79a7fda741e8ef24053db
gayeon00 <chlrkdus4819@gmail.com>
2023-07-21 17:18:27 +0900

    이진탐색

--- INTEGER1920/INTEGER1920.cpp
+++ INTEGER1920/INTEGER1920.cpp
@@ -1,49 +1,47 @@
 ﻿#include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-
+	
 	int N;
 	cin >> N;
 	vector<int> A(N);
+	
 	for (int i = 0; i < N; i++) {
 		cin >> A[i];
 	}
-
 	sort(A.begin(), A.end());
-
+	
 	int M;
 	cin >> M;
+	
 	for (int i = 0; i < M; i++) {
-		int target;
-		cin >> target;
-		int result = 0;
+		bool flag = false;
+		int x;
+		cin >> x;
 
-		int start = 0;
-		int end = N - 1;
+		int start = 0, end = A.size() - 1;
 
 		while (start <= end) {
-			//난 븅시니다....
-			int mid = (start + end) / 2;
-			int midv = A[mid];
+			int med = (start + end) / 2;
 
-			if (target > midv) {
-				start = mid + 1;
-			} else if (target < midv) {
-				end = mid - 1;
+			if (x < A[med]) {
+				end = med - 1;
+			} else if (x > A[med]) {
+				start = med + 1;
 			} else {
-				result = 1;
-				//ㄹㅇ 븅신이다;;;
+				flag = true;
 				break;
 			}
 		}
-
-		cout << result << '\n';
+		
+		if (flag) cout << 1 << '\n';
+		else cout << 0 << '\n';
 	}
+
 }
