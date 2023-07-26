50d9d73b9d0e46642e99436a9368a3746e70430e
gayeon00 <chlrkdus4819@gmail.com>
2023-07-27 00:40:00 +0900

    우선순위 큐 그리디

---
+++ CARD1715/CARD1715.cpp
@@ -0,0 +1,33 @@
+﻿// CARD1715.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    priority_queue<int, vector<int>, greater<int>> A;
+    for (int i = 0; i < N; i++) {
+        int tmp;
+        cin >> tmp;
+        A.push(tmp);
+    }
+
+    int sum = 0;
+    while (A.size() != 1) {
+        int a = A.top();
+        A.pop();
+        int b = A.top();
+        A.pop();
+
+        sum += a + b;
+        A.push(a + b);
+    }
+    cout << sum;
+    
+}
