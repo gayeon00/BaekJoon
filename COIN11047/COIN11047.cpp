b0ebbab59c96f15493bca5cb5793b344da57dfdd
gayeon00 <chlrkdus4819@gmail.com>
2023-07-26 23:58:30 +0900

    총 코인 갯수의 최소 구하기 그리디

---
+++ COIN11047/COIN11047.cpp
@@ -0,0 +1,23 @@
+﻿// COIN11047.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    int N, K;
+    cin >> N >> K;
+
+    int A[10];
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    int sum = 0, cnt = 0;
+    for (int i = N - 1; i >= 0; i--) {
+        while (sum + A[i] <= K) {
+            sum += A[i];
+            cnt++;
+        }
+    }
+    cout << cnt;
+}
