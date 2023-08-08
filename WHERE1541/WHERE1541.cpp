e404cdac6d3c746ae058fa46ef0a3b3c79345a26
gayeon00 <chlrkdus4819@gmail.com>
2023-08-08 15:39:29 +0900

    괄호 적절히 치기

--- WHERE1541/WHERE1541.cpp
+++ WHERE1541/WHERE1541.cpp
@@ -1,50 +1,54 @@
 ﻿#include <iostream>
-#include <string>
-#include <sstream>
 #include <vector>
-
+#include <string>
 using namespace std;
 
-vector<string> split(string& s, const char delimeter) {
-	stringstream ss(s);
-	string single;
-	vector<string> ps;
+vector<int> N;
+vector<char> C;
 
-	while (getline(ss, single, delimeter)) {
-		ps.push_back(single);
+vector<string> splice(string str, char chracter) {
+	vector<string> result;
+	string tmp;
+	for (int i = 0; i < str.size(); i++) {
+		if (str[i] == '-') {
+			result.push_back(tmp);
+			tmp.clear();
+		} else {
+			tmp.push_back(str[i]);
+		}
 	}
+	result.push_back(tmp);
 
-	return ps;
+	return result;
 }
 
-int mySum(string ex) {
-	int sum = 0;
-	vector<string> vs = split(ex, '+');
-	for (string s : vs) {
-		sum += stoi(s);
+int expression_to_int(string expression) {
+	int num = 0, result = 0;
+	for (int i = 0; i < expression.size(); i++) {
+		if (expression[i] == '+') {
+			result += num;
+			num = 0;
+		} else {
+			num = num * 10 + (expression[i]-'0');
+		}
 	}
-	return sum;
+	result += num;
+	return result;
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	string str;
+	cin >> str;
 
-	string ex;
-	cin >> ex;
-
-	vector<string> splitted = split(ex, '-');
-
-	int result=0;
-	for (int i = 0; i < splitted.size(); i++) {
-		int sum = mySum(splitted[i]);
-
-		if (i == 0) {
-			result += sum;
-		} else {
-			result -= sum;
-		}
+	//-를 기준으로 나누기
+	vector<string> spliced = splice(str, '-');
+	//한 덩이 계산하기
+	int result = expression_to_int(spliced[0]);
+	//cout << result << '\n';
+	for (int i = 1; i < spliced.size(); i++) {
+		int tmp = expression_to_int(spliced[i]);
+		//cout << tmp << '\n';
+		result -= tmp;
 	}
 
 	cout << result;
