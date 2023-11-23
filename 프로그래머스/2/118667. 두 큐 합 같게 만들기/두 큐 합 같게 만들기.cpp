af9c0e050334655ea61c784f8f2c0caa17f571ec
gayeon choi <chlrkdus4819@gmail.com>
2023-11-23 15:44:21 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 3.14 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
@@ -0,0 +1,60 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+long total(queue<int> q) {
+    long tot = 0;
+    while(!q.empty()) {
+        tot += q.front();
+        q.pop();
+    }
+    return tot;
+}
+
+int solution(vector<int> queue1, vector<int> queue2) {
+    queue<int> q1;
+    for(int i: queue1) {
+        q1.push(i);
+    }
+    
+    queue<int> q2;
+    for(int i: queue2) {
+        q2.push(i);
+    }
+    
+    int answer = 0;
+    long total1 = total(q1);
+    long total2 = total(q2);
+    if((total1+total2)%2==1) return -1;
+    
+    while(true) {
+        if(answer >= 4*(q1.size()+q2.size())) {
+            answer = -1;
+            break;
+        }
+        if(q1.empty() || q2.empty()) {
+            answer = -1;
+            break;
+        }
+        
+        if(total1 > total2) {
+            q2.push(q1.front());
+            total1 -= q1.front();
+            total2 += q1.front();
+            q1.pop();
+            answer++;
+        } else if(total1 < total2){
+            q1.push(q2.front());
+            total2 -= q2.front();
+            total1 += q2.front();
+            q2.pop();
+            answer++;
+        } else {
+            break;
+        }
+    }
+    
+    return answer;
+}
