2db662c07787a987b30f4e0e5670d24d45d2c789
gayeon choi <chlrkdus4819@gmail.com>
2024-01-17 17:09:50 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 4.04 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    
+    for(int i = 0; i < lost.size(); i++) {
+        for(int j = 0; j < reserve.size(); j++) {
+            if(lost[i] == reserve[j]) { //lost이자 reserve이면 둘 다로부터 제거하기
+                lost.erase(lost.begin()+i);
+                reserve.erase(reserve.begin()+j);
+                i = -1;
+                break;
+            }
+        }
+    }
+    
+    int answer = n - lost.size();
+    sort(lost.begin(), lost.end());
+    sort(reserve.begin(), reserve.end());
+    for(int i = 0; i < lost.size(); i++) {
+        for(int j = 0; j < reserve.size(); j++) {
+            if(lost[i]-1 == reserve[j] || lost[i]+1 == reserve[j]) {
+                answer++;
+                reserve.erase(reserve.begin()+j);
+                break;
+            }
+        }
+    }
+    
+    
+    
+    return answer;
+}
