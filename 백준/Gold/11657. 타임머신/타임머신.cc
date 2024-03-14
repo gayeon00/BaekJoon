21ddb31380eb7fc0bb948db0e936f09912600463
gayeon choi <chlrkdus4819@gmail.com>
2024-03-14 16:17:08 +0900

    [Gold IV] Title: 타임머신, Time: 12 ms, Memory: 2224 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <limits.h>
+
+using namespace std;
+typedef tuple<int, int, int> edge;
+
+int N, M;
+vector<edge> edges;
+vector<long> mdistance;
+
+void read_data() {
+	cin >> N >> M;
+
+	mdistance.resize(N + 1, LONG_MAX);
+
+	for (int i = 0; i < M; i++) {
+		int start, end, time;
+		cin >> start >> end >> time;
+		edges.push_back(make_tuple(start, end, time));
+	}
+}
+
+void solve() {
+	mdistance[1] = 0;
+
+	for (int i = 0; i < N - 1; i++) {
+		for (int j = 0; j < M; j++) {
+			edge curr = edges[j];
+			int start = get<0>(curr);
+			int end = get<1>(curr);
+			int time = get<2>(curr);
+
+			if (mdistance[start] != LONG_MAX && mdistance[end] > mdistance[start] + time) {
+				mdistance[end] = mdistance[start] + time;
+			}
+		}
+	}
+
+	bool flag = true;
+
+	for (int j = 0; j < M; j++) {
+		edge curr = edges[j];
+		int start = get<0>(curr);
+		int end = get<1>(curr);
+		int time = get<2>(curr);
+
+		if (mdistance[start] != LONG_MAX && mdistance[end] > mdistance[start] + time) {
+			flag = false;
+			break;
+		}
+	}
+	
+	if (flag) {
+		for (int i = 2; i <= N; i++) {
+			if (mdistance[i] != LONG_MAX) cout << mdistance[i] << '\n';
+			else cout << -1 << '\n';
+		}
+	} else {
+		cout << -1 << '\n';
+	}
+}
+
+int main() {
+	read_data();
+	solve();
+	return 0;
+}
