a971eb5fb3bcb3f08c355ce6d36fb019ea3c89d3
gayeon choi <chlrkdus4819@gmail.com>
2024-03-23 01:27:59 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -5,26 +5,26 @@
 using namespace std;
 
 bool compare(const string& a, const string& b) {
-    return a+b > b+a;
+    return b+a<a+b;
 }
 
 string solution(vector<int> numbers) {
     string answer = "";
-    
-    vector<string> strs;
-    for(auto i: numbers) {
-        strs.push_back(to_string(i));
+    vector<string> strNumbers;
+    for(int num: numbers) {
+        strNumbers.push_back(to_string(num));
     }
     
-    sort(strs.begin(), strs.end(), compare);
+    sort(strNumbers.begin(), strNumbers.end(), compare);
     
-    for(auto i: strs) {
-        answer += i;
-    }
-    
-    if(answer[0] == '0') {
+    if(strNumbers[0]=="0") {
         answer = "0";
+    } else {
+        for(string str: strNumbers) {
+            answer.append(str);
+        }
     }
+    
     
     return answer;
 }
